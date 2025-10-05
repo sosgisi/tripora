5de648bfb1c8aed240ca055bdc7ab7571d36cfa4
Shougi <116136870+sosgisi@users.noreply.github.com>
2025-10-05 15:39:22 +0700

    fix type error 9

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,7 @@
 'use client'
 
+export const dynamic = "force-dynamic";
+
 import { Footer } from "@/components/footer";
 import { Navbar } from "@/components/navbar"
 import { ChevronLeft, ChevronRight, Shield, Star, Users } from "lucide-react";
